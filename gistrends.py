02747e9e76c7e5999f6cfdf7b9f2208b3ee8781a
simonscheider <simonscheider@web.de>
2017-11-15 20:17:15 +0100

    generated toollist in RDF

--- gistrends.py
+++ gistrends.py
@@ -16,7 +16,7 @@ import csv
 #import arcpy
 import json
 import re
-import urllib3
+import urllib
 import rdflib
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
@@ -29,6 +29,8 @@ import operator
 from altair import *
 from IPython.display import display
 import pandas
+
+from SPARQLWrapper import SPARQLWrapper, JSON
 
 import requests
 from bs4 import BeautifulSoup
@@ -153,7 +155,6 @@ def getGISSoftwareList():
 
 
 def getDBpediaCompany(url):
-    from SPARQLWrapper import SPARQLWrapper, JSON
     sparql = SPARQLWrapper("http://dbpedia.org/sparql")
     sparql.setReturnFormat(JSON)
 
@@ -169,34 +170,19 @@ def getDBpediaCompany(url):
 
 
 
-
-
-
-def buildArcToolList():
-    print ('Number of Tools '+str(len(arcpy.ListTools())))
-    toolboxes = arcpy.ListToolboxes()
-    out = {}
-    outf = 'ArcGIStooldict.json'
-    for toolbox in toolboxes:
-        print toolbox
-        out[toolbox]=[]
-        tools = arcpy.ListTools('*_'+toolbox[toolbox.index('(')+1:toolbox.index(')')])
-        for tool in tools:
-            #print tool
-            out[toolbox].append(tool)
-    with open(outf, 'w') as fp:
-        json.dump(out, fp)
-    fp.close
-    return outf
-
-def readToolBoxes(tooldictfile, listoftoolboxes):
+def readToolBoxes(tooldictfile, listoftoolboxes=[]):
     tdict = {}
     with open(tooldictfile) as tooldictf:
         tooldict = json.load(tooldictf)
-        for t in listoftoolboxes:
-           tdict[t]=tooldict[t]
+        if listoftoolboxes != []:
+            for t in listoftoolboxes:
+               tdict[t]=tooldict[t]
+        else:
+            tdict = tooldict
     tooldictf.close
     return tdict
+
+
 
 def getTrends4Tools(tooldict,referencekeyword):
     gt = GatherTools(referencekeyword)
@@ -275,13 +261,117 @@ def visualize(resultdump, twolayered =True, index ='ArcGIS Tools'):
 
 
 
+################gather tools and software
+
+def buildArcToolList():
+    print ('Number of Tools '+str(len(arcpy.ListTools())))
+    toolboxes = arcpy.ListToolboxes()
+    out = {}
+    outf = 'ArcGIStooldict.json'
+    for toolbox in toolboxes:
+        print toolbox
+        out[toolbox]=[]
+        tools = arcpy.ListTools('*_'+toolbox[toolbox.index('(')+1:toolbox.index(')')])
+        for tool in tools:
+            #print tool
+            out[toolbox].append(tool)
+    with open(outf, 'w') as fp:
+        json.dump(out, fp)
+    fp.close
+    return outf
+
 
 
 
 def normalizeArcpyToolString(tool):
         #ex = 'CreateSchematicFolder_schematics'
         #Get the actual toolname (without toolbox and splitting at upper case letters)
-        return ' '.join(re.findall('[A-Z][^A-Z]*', tool.partition('_')[0]))
+        return ' '.join(re.findall('[A-Z][A-Z]*[a-z]*[0-9]*', tool.partition('_')[0]))
+
+
+
+
+def generateRDF(outf, softuri, tooldictf, softdictf=None):
+    from rdflib import URIRef, BNode, Literal, Namespace, Graph
+    from rdflib.namespace import RDF, FOAF, RDFS
+
+    dbo = Namespace("http://dbpedia.org/ontology/")
+    dbp = Namespace("http://dbpedia.org/resource/")
+    dc = Namespace("http://purl.org/dc/elements/1.1/")
+    dct = Namespace("http://purl.org/dc/terms/")
+    wf = Namespace("http://geographicknowledge.de/vocab/Workflow.rdf#")
+    gis = Namespace("http://geographicknowledge.de/vocab/GISConcepts.rdf#")
+    tools = Namespace("http://geographicknowledge.de/vocab/GISTools.rdf#")
+
+
+    tdict = readToolBoxes(tooldictf)
+    g = Graph()
+
+
+    if softdictf != None:
+        softdict = readSoft(softdictf)
+        softwarelist = []
+        for software,v in softdict.items():
+            softwarelist.append(software)
+            g.add((URIRef(software), RDF.type, dbo.Software))
+            if 'name' in v.keys():      g.add((URIRef(software), FOAF['name'], Literal(v['name'])))
+            if 'website' in v.keys():   g.add((URIRef(software),FOAF['isPrimaryTopicOf'], URIRef(v['website'])))
+            if v['companies']!= []:     g.add((URIRef(software), dbo.developer, URIRef(v['companies'][0])))
+    else: #there is already some software tools, then load them
+        g.parse(outf, format='turtle')
+
+    w = getWebsite(softwarelist)
+    if w != []:
+        for ww in w:
+            g.add((URIRef(ww[1]),FOAF['homepage'],URIRef(ww[0])))
+
+    #Now add the tools of some software softuri
+    if URIRef(softuri) in g.all_nodes():
+        for toolbox, toollist in tdict.items():
+            tb = urllib.pathname2url(toolbox)
+            g.add((tools[tb], RDF.type, gis.Toolbox))
+            g.add((tools[tb], dct.isPartOf, URIRef(softuri)))
+            g.add((tools[tb], FOAF.name, Literal(toolbox)))
+            for t in toollist:
+                g.add((tools[t], RDF.type, gis.Tool))
+                g.add((tools[t], dct.isPartOf, tools[tb]))
+                g.add((tools[t], FOAF.name, Literal(normalizeArcpyToolString(t))))
+
+    g.bind('dbo', URIRef("http://dbpedia.org/ontology/"))
+    g.bind('dbp', URIRef("http://dbpedia.org/resource/"))
+    g.bind('dc', URIRef("http://purl.org/dc/elements/1.1/"))
+    g.bind('dct', URIRef("http://purl.org/dc/terms/"))
+    g.bind('wf', URIRef("http://geographicknowledge.de/vocab/Workflow.rdf#"))
+    g.bind('gis', URIRef("http://geographicknowledge.de/vocab/GISConcepts.rdf#"))
+    g.bind('tools', URIRef("http://geographicknowledge.de/vocab/GISTools.rdf#"))
+    g.bind('foaf',FOAF)
+    g.bind('rdf',RDF)
+    g.bind('rdfs', RDFS)
+    print 'number of triples generated: '+str(len(g))
+    g.serialize(destination=outf, format='turtle')
+
+
+
+def getWebsite(urllist):
+    sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+    sparql.setReturnFormat(JSON)
+    tl ='<'+'>, <'.join(urllist)+'>'
+    query = """
+    Select ?f ?tool WHERE {
+    ?tool <http://xmlns.com/foaf/0.1/homepage> ?f.
+    FILTER(?tool in ("""+tl+"""))
+    }
+            """
+    sparql.setQuery(query)  # the previous query as a literal string
+
+    r = sparql.query().convert()
+    com = []
+    for f in r['results']['bindings']:
+                    com.append((f['f']['value'],f['tool']['value']))
+    return com
+
+
+
 
 
 
@@ -295,7 +385,7 @@ def main():
 
     #td = readToolBoxes('ArcGISTooldict.json', ['Spatial Analyst Tools(sa)','Conversion Tools(conversion)','Analysis Tools(analysis)'])
     #res = getTrends4Tools(td,'ArcGIS')
-    visualize('GTresults_kwArcGIS.json')
+    #visualize('GTresults_kwArcGIS.json')
     #kw_list=['ArcGIS', 'GRASS GIS', 'QGIS', 'R studio', 'Interpolation']#'MapInfo']'ILWIS'
     #kw_list = ['ArcMap','Extract by Mask', 'Set Null', 'IDW', 'Raster calculator']#['zonal', 'areal interpolation', 'raster calculator', 'ArcGIS' ]
 
@@ -312,7 +402,7 @@ def main():
 ##            i+=1
 ##            if i ==4: break
 
-
+    generateRDF('GISTools.ttl','http://dbpedia.org/resource/ArcGIS','ArcGISTooldict.json','GISSoftdict.json') #a gis:Toolbox
 
 
 
