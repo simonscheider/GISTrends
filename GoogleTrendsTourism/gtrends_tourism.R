4438d6ddfaa5c35e8826ed0fb870e7afe46ab307
Andrea Ballatore <andrea.ballatore@gmail.com>
2018-11-04 14:46:09 +0000

    started analysis

--- GoogleTrendsTourism/gtrends_tourism.R
+++ GoogleTrendsTourism/gtrends_tourism.R
@@ -76,8 +76,8 @@ get_url = function( url ){
 get_url_piavpn = function( url ){
   library(RCurl)
   #print(paste('get_url_piavpn',url))
-  pia_socks5 = 'socks5h://x1936726:RYWxEUiy6N@proxy-nl.privateinternetaccess.com:1080'
-  #pia_socks5 = 'socks5://x1936726:RYWxEUiy6N@proxy-nl.privateinternetaccess.com:1080'
+  pia_socks5 = 'socks5h://XXXXX@proxy-nl.privateinternetaccess.com:1080'
+  #pia_socks5 = 'socks5://XXXXX@proxy-nl.privateinternetaccess.com:1080'
   options(RCurlOptions = list(proxy = pia_socks5,
                               #useragent = "Mozilla",
                               followlocation = TRUE,
@@ -99,163 +99,12 @@ load_google_domains <- function(){
   google_domains$TOPDOMAIN = gsub('\\.','',google_domains$TOPDOMAIN)
   return(google_domains)
 }
-#google_domains = load_google_domains()
-
-#google_languages = scan('data/google/google_supported_languages.txt',sep = '\n',what = "character")
-#stopifnot(length(google_languages)==45)
-
-#google_country_langs = read.table('data/google/google_country_langs.txt', header = F, sep = '\t', na.strings = '')
-#names(google_country_langs) = c('COUNTRY','LANGS')
-#google_country_langs$LANGS = as.character(google_country_langs$LANGS)
 
 get_google_languages_for_country <- function(country_code){
   stopifnot(nchar(country_code)==2)
   print(paste('get_google_languages_for_country',country_code))
   langs = strsplit( subset(google_country_langs, google_country_langs$COUNTRY==country_code)$LANGS, ',')[[1]]
   return(langs)
-}
-
-get_google_baseurl_country <- function( country_code ){
-  stopifnot(nchar(country_code)==2)
-
-  # handle exceptions
-  res = ''
-  if (country_code == "US") res = "http://www.google.com"
-  if (country_code == "GB") res = "http://www.google.co.uk"
-  if (country_code == "UK") res = "http://www.google.co.uk"
-  if(nchar(res)>0) return(res)
-
-  res = subset(google_domains, toupper(google_domains$TOPDOMAIN) == country_code)$DOMAIN
-  #print(res)
-  stopifnot(length(res)==1)
-
-  res = paste0("http://www",res)
-  return(res)
-}
-
-parse_google_serp = function( df, html ){
-  # Based on https://github.com/hadley/rvest
-  require(rvest)
-  stopifnot(nrow(df)==1)
-  serp_page = read_html(html)
-
-  # get basic Google SERP
-  serp_urls = serp_page %>%
-    html_nodes(xpath = "//div[@id='search']//h3/a") %>%
-    html_attr("href")
-  serp_titles = serp_page %>%
-    html_nodes(xpath = "//div[@id='search']//h3/a") %>%
-    html_text()
-  if (length(serp_urls)==0) print(">>> Possible crawler detection <<<")
-  else print("> SERP found <")
-  stopifnot(length(serp_urls)>0)
-  stopifnot(length(serp_titles)>0)
-
-  serpdf = data.frame( FULL_RESULT=serp_urls, SEARCH_UID=df$SEARCH_UID )
-  serpdf$TITLE = serp_titles
-  serpdf = build_serp_df(serpdf)
-  serpdf$ORGANIC = NULL
-  serpdf$RES_TYPE = 'core'
-  serpdf$DESC = NA
-  stopifnot(nrow(serpdf)>0)
-
-  cols = c("SEARCH_UID","RES_TYPE","RANK","RESULT_URL","TITLE","DESC","FULL_RESULT")
-  allserpdf = serpdf[,cols]
-
-  # NEWS
-  serp_urls = serp_page %>%
-    html_nodes(xpath = "//div[@id='search']//td[@valign='top']/div/a") %>%
-    html_attr("href")
-
-  if (length(serp_urls)>0){
-    serp_titles = serp_page %>%
-      html_nodes(xpath = "//div[@id='search']//td[@valign='top']/div/a") %>%
-      html_text()
-    #print(paste("news serp_urls",length(serp_urls)))
-    #print(paste("news serp_urls",length(serp_urls)))
-
-    serpnewsdf = data.frame(FULL_RESULT=serp_urls, TITLE=serp_titles, SEARCH_UID=df$SEARCH_UID)
-    serpnewsdf = build_serp_df(serpnewsdf)
-    serpnewsdf$ORGANIC = NULL
-    serpnewsdf$RES_TYPE = 'news'
-    serpnewsdf$DESC = NA
-
-    allserpdf = rbind(allserpdf, serpnewsdf[,cols])
-  }
-
-  # IMAGES
-  image_query = "//div[@id='search']//img[@align]"
-  titles = serp_page %>% html_nodes(xpath = image_query) %>% html_attr('title')
-  urls = serp_page %>% html_nodes(xpath = image_query) %>% html_attr('src')
-  alts = serp_page %>% html_nodes(xpath = image_query) %>% html_attr('alt')
-  images_found = F
-  if (length(titles)>0){
-    imgdf = data.frame(TITLE = titles, RESULT_URL = urls, FULL_RESULT = urls, DESC = alts, SEARCH_UID=df$SEARCH_UID )
-    imgdf$RANK = seq(nrow(imgdf))
-    imgdf$RES_TYPE = 'images'
-    images_found = T
-
-    allserpdf = rbind(allserpdf, imgdf[,cols])
-  }
-  rm(titles,urls,alts)
-
-  # RELATED SEARCHES
-  relsearch_query = "//td//p[@class='aw5cc']"
-  queries = serp_page %>% html_nodes(xpath = relsearch_query) %>% html_text()
-  urls = serp_page %>% html_nodes(xpath = "//td//p[@class='aw5cc']//a") %>% html_attr('href')
-  if (length(urls)>0){
-    # related researches found
-    reldf = data.frame(TITLE = queries, RESULT_URL = NA, FULL_RESULT = urls, DESC = NA, SEARCH_UID=df$SEARCH_UID )
-    reldf$RANK = seq(nrow(reldf))
-    reldf$RES_TYPE = 'related_search'
-    baseurl = get_google_baseurl_country( as.character(unique(df$QUERY_BASECOUNTRY)) )
-    reldf$RESULT_URL = paste0( baseurl, sapply( reldf$FULL_RESULT, clean_google_url ) )
-    #View(reldf)
-    allserpdf = rbind(allserpdf, reldf[,cols])
-  }
-  rm(queries)
-  rm(urls)
-
-  # ADS
-  #ad_query = "//div[@id='tads']//li"
-  #ads = serp_page %>% html_nodes(xpath = ad_query) %>% html_text()
-
-  #serpimgdf = build_serp_df(serp_urls)
-
-  # SEMANTIC LINKS
-  sem_query_poi = "//div[@class='dXAUyb']//a" # POINTS of INTERESTS
-  #sem_query2 = "//a[@class='FEM55']//img"
-  urls = serp_page %>% html_nodes(xpath = sem_query_poi) %>% html_attr('href')
-  if (length(urls)>0){
-    stopifnot(!is.na(urls))
-    alts = serp_page %>% html_nodes(xpath = sem_query_poi) %>% html_text()
-    #alts = serp_page %>% html_nodes(xpath = sem_query2) %>% html_attr('title')
-    semdf = data.frame(TITLE = trimws(alts), RESULT_URL = NA, FULL_RESULT = urls, DESC = NA )
-    semdf = subset(semdf, semdf$TITLE!='')
-    semdf$SEARCH_UID=unique(df$SEARCH_UID)
-    semdf$RANK = seq(nrow(semdf))
-    semdf$RES_TYPE = 'semantic_link'
-    baseurl = get_google_baseurl_country( as.character(unique(df$QUERY_BASECOUNTRY)) )
-    semdf$RESULT_URL = paste0( baseurl, sapply( semdf$FULL_RESULT, clean_google_url ) )
-    #View(semdf)
-
-    # SEMANTIC DESCRIPTIONS
-    sem_query = "//div[@class='V7Q8V']"
-    sem_desc = serp_page %>% html_nodes(xpath = sem_query) %>% html_text(trim = T)
-    sem_desc = gsub('\t','  ',sem_desc)
-    sem_desc = gsub('\n','  ',sem_desc)
-    semdescdf = data.frame(TITLE = NA, RESULT_URL = NA, FULL_RESULT = NA, DESC = sem_desc )
-    semdescdf = subset(semdescdf, semdescdf$DESC!='')
-    stopifnot(nrow(semdescdf)>0)
-    semdescdf$SEARCH_UID=unique(df$SEARCH_UID)
-    semdescdf$RANK = seq(nrow(semdescdf))
-    semdescdf$RES_TYPE = 'semantic_desc'
-    #View(semdescdf)
-    stopifnot(nrow(semdescdf)>0)
-    allserpdf = rbind(allserpdf, semdf[,cols])
-    allserpdf = rbind(allserpdf, semdescdf[,cols])
-  }
-  return(allserpdf)
 }
 
 # Google Trends ---------------------------------
@@ -392,6 +241,11 @@ qua = readOGR(dsn="geodata/MetrAWijk.shp")
 names(qua)[1:2]=cols
 nei = readOGR(dsn="geodata/MetrABuurt.shp")
 names(nei)[1:2]=cols
+
+# save geodata as R data
+saveRDS( muni, file = 'geodata/netherlands_municipalities_sdf.rds', compress=T )
+saveRDS( qua, file = 'geodata/netherlands_quarters_sdf.rds', compress=T )
+saveRDS( nei, file = 'geodata/netherlands_neighbourhoods_sdf.rds', compress=T )
 
 # init data
 interest_over_time_df = data.frame()
