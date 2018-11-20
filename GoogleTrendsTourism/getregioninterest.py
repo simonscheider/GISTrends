4fff1d07ef73aac249146f48c4ecb8e514ae2c25
Scheider <s.scheider@uu.nl>
2018-11-20 17:51:47 +0100

    generated targetregioninterest

--- GoogleTrendsTourism/getregioninterest.py
+++ GoogleTrendsTourism/getregioninterest.py
@@ -11,6 +11,7 @@
 
 import pytrends
 import pandas
+import numpy as np
 
 from pytrends.request import TrendReq
 
@@ -24,33 +25,70 @@ class GatherInterest():
     def __init__(self, referencekeyword, unsaferun=False):
          self.results = {}
          self.kw = referencekeyword
+         self.kwvalues = {}
+         self.results['reference']=referencekeyword
          self.currentkw= [referencekeyword]
          self.unsaferun =unsaferun
 
 
     def reset(self):
         self.currentkw= [self.kw]
+        #self.kw = sortedkw[3]
 
-    def add(self,kw):
-        self.currentkw.append(kw)
+    def add(self,kw, region):
+        if kw not in self.currentkw and kw != 'NaN' and kw != None and type(kw) is str:
+            print str(kw) +':'+ str(type(kw))
+            self.currentkw.append(kw)
         if len(self.currentkw)>=5:
-            res = self.queryGTrends(self.currentkw)
-            self.store(res)
+            print 'querying for 5 keywords!'
+            res = self.queryGTrends(self.currentkw)[self.currentkw]
+            #print list(res)
+            self.kwvalues = res['Amsterdam'].to_json()
+            sortedvalues = res.max().sort_values()
+            sortedkw = sortedvalues.keys().tolist()
+            print sortedkw
+            print sortedvalues
+            #print sortedvalues.iloc[-1]
+            for id in range(1,sortedvalues.size):
+                last =-id
+                secondlast = -(id+1)
+                if sortedvalues.iloc[last]- sortedvalues.iloc[secondlast]>80:
+                    newvalues = self.queryGTrends(sortedkw[0:last])[sortedkw[0:last]]
+                    #.astype(float)
+                    newmax = newvalues.max().sort_values().keys()[-1]
+                    #print newmax
+                    newreferencevalue =float(sortedvalues.get_value(newmax))
+                    #print str(newreferencevalue)
+                    print 'Re-queried with higher resolution! reference '+newmax + " "+str(newreferencevalue)
+                    newvalues = newvalues*newreferencevalue/100
+                    #print newvalues
+                    res = res[sortedkw[last:]].join(newvalues)
+                    break
+            #print res
+            self.store(res,region)
             self.reset()
 
-    def store(self,res):
+    def store(self,res, region):
         for id,t in enumerate(self.currentkw):
             if not t == self.kw:
                 #make sure trends are within plausible limits
-                B = res[t] < res[self.kw] and res[t] >0
-                if self.unsaferun: B = True
-                if B:  self.results[t]=str(res[t])
+                #B = res[t] < res[self.kw] and res[t] >0
+                #if self.unsaferun: B = True
+                self.results[t]=[region,res[t]]
 
 
-    def dump(self,res = 'data\\targetqueries\\GTresults.json'):
+
+    def dump(self,referenceid, res = 'data\\targetqueries\\GTresults.json'):
+        self.results[self.kw]=[referenceid,self.kwvalues]
+        dumres = {}
+        for k,v in self.results.items():
+            if type(v) is list:
+                dumres[k]=[v[0],v[1].to_json()]
+            else:
+                dumres[k]=v
         print "dumped items: "+str(len(self.results))
         with open(res, 'w') as fp:
-            json.dump(self.results, fp)
+            json.dump(dumres, fp)
         fp.close
 
     def queryGTrends(self,kw_list):
@@ -82,25 +120,55 @@ class GatherInterest():
         # Get Google Keyword Suggestions
         #suggestions_dict = pytrends.suggestions(keyword='GIS')
         # print(suggestions_dict)
-        return time.mean()
+        return time#.mean()
 
-"""Trends for tools"""
-def getTrends4Regions(regioncsv,referencekeyword):
-    gt = GatherInterest(referencekeyword)
+"""Trends for regions"""
+def getTrends4Regions(regioncsv, referencekeyword):
     count = 0
+    keywordid = ''
     pd = pandas.read_csv(regioncsv)
-    pd = pd[pd['QUERY_BASETERM'] == False]
-    print "ready to query over "+str(pd.size) + ' regions!'
-    print pd
+    pd = pd[pd['QUERY_BASETERM'] == False & pandas.notnull(pd['safe_keyword'])]
+    gt = GatherInterest(referencekeyword)
+    print "ready to query over "+str(len(pd.index)) + ' regions!'
+    print list(pd)
+    #print pd
+    #kws = []
     for index, row in pd.iterrows():
-        count+=1
-        print row['safe_keyword']
-        #gt.add()
+        kw =  row['safe_keyword']
+        region = row['\xef\xbb\xbfGEOUNIT_CODE']
+        if kw == referencekeyword: keywordid= region
+        if kw != 'nan':
+            #kws.append(kw)
+            count+=1
+            gt.add(kw, region)
+            #
+        #if count == 10 :
+            #break
+
     print 'Regions queried '+str(count)
-    gt.dump('data\\targetqueries\\GT'+referencekeyword+'.json')
+    #gt.dump(keywordid,'data\\targetqueries\\GT.json')
+    return gt.results
+
+def analyse(results):
+    for k,v in results.items():
+        if k != 'reference':
+            name = k
+            region = v[0]
+            data = v[1]
+            print data.groupby(pandas.TimeGrouper('A')).mean()
+            break
+
+
+
+
+
+
 
 def main():
-    getTrends4Regions('geodata\\GMsearchterms.csv','Amsterdam')
+    results = getTrends4Regions('geodata\\GMsearchterms.csv', 'Amsterdam')
+    #orderregions(['Amsterdam',  'Almere','Aalsmeer','Amstelveen'])
+    analyse(results)
+
 
 if __name__ == '__main__':
     main()
